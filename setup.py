4a6c6d5e3631f60f1db824b9d8a2637dd2730480
Alexander Lenz <alexander.lenz@frm2.tum.de>
2014-05-30 15:42:56 +0200

    setup.py: Fix module authors mail.

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@
 # 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #
 # Module authors:
-#   Alexander Lenz <alexander.lenz@frm2.tum.de>
+#   Alexander Lenz <fslenz@gmail.com>
 #
 # *****************************************************************************
 
